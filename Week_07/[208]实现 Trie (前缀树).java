4e4cefc0d7322521121084d3c9f14bc513e6788d
yuqiu-pp <41912446@qq.com>
2020-06-07 11:59:38 +0800

    update

---
+++ Week_07/[208]实现 Trie (前缀树).java
@@ -0,0 +1,89 @@
+//实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。 
+//
+// 示例: 
+//
+// Trie trie = new Trie();
+//
+//trie.insert("apple");
+//trie.search("apple");   // 返回 true
+//trie.search("app");     // 返回 false
+//trie.startsWith("app"); // 返回 true
+//trie.insert("app");   
+//trie.search("app");     // 返回 true 
+//
+// 说明: 
+//
+// 
+// 你可以假设所有的输入都是由小写字母 a-z 构成的。 
+// 保证所有输入均为非空字符串。 
+// 
+// Related Topics 设计 字典树
+
+package leetcode.editor.cn;
+
+class LC208{
+    public static void main(String[] args) {
+        // Solution solution = new LC208().new Solution();
+        // TO TEST
+        // System.out.println(solution.实现 Trie (前缀树)());
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Trie {
+    private TrieNode root;
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+        root.val = ' ';
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return node.isWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
